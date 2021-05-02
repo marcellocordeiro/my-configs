814acca1ed882b43aa91d2ac2c2bd05aa3a1e8dd
Marcello Cordeiro <msc3@cin.ufpe.br>
2021-05-02 04:37:31 -0300

    Add Arch Linux

---
+++ archlinux/config_kdewallet_git.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+ssh_add_script_file=~/.config/autostart-scripts/ssh-add.sh
+ssh_add_script=$(cat <<'EOF'
+#!/bin/sh
+
+ssh-add -q ~/.ssh/id_ed25519 < /dev/null
+EOF
+)
+
+ssh_env_script_file=~/.config/plasma-workspace/env/ssh-env.sh
+ssh_env_script=$(cat <<'EOF'
+#!/bin/sh
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export SSH_ASKPASS='/usr/bin/ksshaskpass'
+EOF
+)
+
+echo "${ssh_add_script}" > "${ssh_add_script_file}"
+echo "${ssh_env_script}" > "${ssh_env_script_file}"
+
+chmod +x "${ssh_add_script_file}"
+chmod +x "${ssh_env_script_file}"
+
+# ssh-agent service
+ssh_agent_start_service_file=~/.config/systemd/user/ssh-agent.service
+ssh_agent_start_service=$(cat <<'EOF'
+[Unit]
+Description=SSH key agent
+
+[Service]
+Type=simple
+Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
+# DISPLAY required for ssh-askpass to work
+Environment=DISPLAY=:0
+ExecStart=/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK
+
+[Install]
+WantedBy=default.target
+EOF
+)
+
+mkdir -p ~/.config/systemd/user
+echo "${ssh_agent_start_service}" > "${ssh_agent_start_service_file}"
+systemctl enable --user ssh-agent.service
---
+++ archlinux/plasma_env.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+path_script_file=~/.config/plasma-workspace/env/path.sh
+path_script=$(cat <<'EOF'
+#!/bin/sh
+
+# Android
+export ANDROID_HOME=$HOME/Android/Sdk
+export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+
+#export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/emulator
+
+# npm
+export PATH=$PATH:$HOME/.local/bin
+export npm_config_prefix=$HOME/.local
+EOF
+)
+
+echo "${path_script}" > "${path_script_file}"
+
+chmod +x "${path_script_file}"
