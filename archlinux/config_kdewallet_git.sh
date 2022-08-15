9ae387a5d068a2e8d87837dcf896dd2519c67b17
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2022-08-15 16:09:55 -0300

    [archlinux] Simplify ssh setup

--- archlinux/config_kdewallet_git.sh
+++ archlinux/config_kdewallet_git.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-ssh_add_autostart_file=~/.config/autostart/ssh-add.desktop
-ssh_add_autostart=$(cat <<'EOF'
-[Desktop Entry]
-Exec=ssh-add -q ~/.ssh/id_ed25519
-Name=ssh-add
-Type=Application
-EOF
-)
-
 ssh_env_script_file=~/.config/plasma-workspace/env/ssh-env.sh
 ssh_env_script=$(cat <<'EOF'
 #!/bin/sh
 
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export SSH_ASKPASS='/usr/bin/ksshaskpass'
+export SSH_ASKPASS_REQUIRE=prefer
 EOF
 )
 
-echo "${ssh_add_autostart}" > "${ssh_add_autostart_file}"
 echo "${ssh_env_script}" > "${ssh_env_script_file}"
 
 chmod +x "${ssh_env_script_file}"
@@ -44,3 +35,13 @@ EOF
 mkdir -p ~/.config/systemd/user
 echo "${ssh_agent_start_service}" > "${ssh_agent_start_service_file}"
 systemctl enable --user ssh-agent.service
+
+ssh_config_file=~/.ssh/config
+ssh_config=$(cat <<'EOF'
+Host github.com
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_ed25519
+EOF
+)
+
+echo "${ssh_config}" > "${ssh_config_file}"
